3fff2fa21daf5072046436fc6931359ba460884f
rishu9354 <rs682688@gmail.com>
2025-05-09 12:25:13 +0530

    updated contact-form

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,20 +1,20 @@
 <template>  
     <div class="min-h-screen flex items-center justify-center p-4 bg-gradient-to-br from-blue-100 to-violet-300">
-        <div class="bg-white/30 backdrop-blur-md rounded-2xl shadow-2xl flex overflow-hidden max-w-4xl w-full">
-            
-            <div class="flex items-center justify-center w-full md:w-1/2 bg-white/40 p-6">
+        <div class="bg-white/30 backdrop-blur-md rounded-2xl shadow-2xl flex flex-col md:flex-row overflow-hidden max-w-4xl w-full">
+             
+            <div class="hidden md:flex items-center justify-center md:w-1/2 bg-white/40 p-6">
                 <img src="/images/Contact-us.png" alt="contact-us-pic" class="w-96 h-auto object-contain">
             </div>
 
-            <div class="w-full md:w-1/2 p-8">
-                <h2 class="text-3xl font-bold text-center text-gray-800 mb-8">Contact-us</h2>
+            <div class="w-full md:w-1/2 p-8 sm:p-8 bg-white/60">
+                <h2 class="text-2xl sm:text-3xl font-bold text-center text-gray-800 mb-8 sm:mb-8">Contact-us</h2>
 
                
-                <form action="" @submit.prevent="onSubmit" class="space-y-6">
+                <form action="" @submit.prevent="onSubmit" class="space-y-5">
                     <div>
                         <label for="name" class="block text-sm font-medium text-gray-700 mb-1">Name</label> 
                         <input type="text" name="name" placeholder="Enter your Name"
-                        class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none" v-model="form.name">
+                        class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none" v-model="form.name" autofocus>
 
                     </div>
                     <div>   
@@ -36,7 +36,7 @@
                         <button type="submit"
                         class="w-full bg-violet-600 text-white py-2 rounded-lg hover:bg-violet-800 transition duration-300 font-semibold mt-7">
                         <span v-if="isLoading">Loading..</span>
-                        <span v-else>Send</span>
+                        <span v-else>Send Message</span>
 
                         </button>
                 </form>
